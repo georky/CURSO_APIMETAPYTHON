2709f5c544910b70a2ffbc08d225296661ecafdf
Georky Condoy <georky27condoy@gmail.com>
2024-07-13 23:03:50 -0500

    webhook_1

--- app.py
+++ app.py
@@ -42,6 +42,26 @@ def agregar_mensajes_log(texto):
     db.session.add(nuevo_registro)
     db.session.commit()
 
+#Token de verificacion para la configuracion
+TOKEN_ANDERCODE = "ANDERCODE"
+
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request.method == 'GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method == 'POST':
+        reponse = recibir_mensajes(request)
+        return reponse
+
+def verificar_token(req):
+    token = req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token == TOKEN_ANDERCODE:
+        return challenge
+    else:
+        return jsonify({'error':'Token Invalido'}),401
 def recibir_mensajes(req):
    
         req = request.get_json()
